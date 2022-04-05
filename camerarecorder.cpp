e92575462765f4971d739bf3d8d74f472b261375
JSK <jsk@nkom.no>
2022-04-05 13:06:40 +0200

    FFMpeg lib test

--- camerarecorder.cpp
+++ camerarecorder.cpp
@@ -106,8 +106,7 @@ void CameraRecorder::selectCamera()
             {
                 sws_scale(img_convert_ctx, picture->data, picture->linesize, 0,
                           codec_ctx->height, picture_rgb->data, picture_rgb->linesize);
-                std::stringstream file_name;
-                file_name << "test" << cnt << ".ppm";
+                QString file_name = "test.ppm";
                 output_file.open(file_name.str().c_str());
                 output_file << "P3 " << codec_ctx->width << " " << codec_ctx->height
                             << " 255\n";
