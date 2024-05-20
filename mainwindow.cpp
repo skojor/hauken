372f897c2a6a9aac216f257457ac6a48a88a59ac
Jørn Skorstad <jsk@nkom.no>
2024-05-20 16:59:55 +0200

    Updated program version

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1102,6 +1102,7 @@ void MainWindow::changelog()
     QString txt;
     QTextStream ts(&txt);
     ts << "<table>"
+       << "<tr><td>2.38</td><td>Added GNSS data display in separate window</td></tr>"
        << "<tr><td>2.37</td><td>Instrument list collected from external server if available</td></tr>"
        << "<tr><td>2.36</td><td>MQTT data can be added to incident log</td></tr>"
        << "<tr><td>2.35</td><td>Watchdog function improved. Spectrum mask bugfix. Foreign letters bugfix</td></tr>"
