b498604c50bc63a03059f459dac7bef8a069fffc
JSK <jsk@nkom.no>
2025-10-28 13:46:20 +0100

    Updated naming convention for class Mqtt

--- mqtt.h
+++ mqtt.h
@@ -7,7 +7,6 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonArray>
-#include <QProcess>
 #include "config.h"
 #include "typedefs.h"
 #include "networkrequestsbase.h"
@@ -22,6 +21,7 @@ enum SITESTATUS {
 
 #define MQTT_CONN_TIMEOUT_MS    30000
 #define MQTT_DATATRANSFER_TIMEOUT_MS 120000
+#define WEBSWITCH_TEMP_INTERVAL_MS 60000
 
 class Mqtt : public NetworkRequestsBase
 {
@@ -33,7 +33,7 @@ public slots:
     void updSettings();
 
 signals:
-    void newData(QString& name, double& value);
+    void newData(const QString, double);
     void toIncidentLog(const NOTIFY::TYPE, const QString, const QString);
     void triggerRecording();
     void endRecording();
@@ -43,26 +43,23 @@ private slots:
     void checkConnection();
     void error(QMqttClient::ClientError error);
     void msgSent(qint32 id);
-    //void sendMessage(const QVector<Sensor> &sensorList);
     void subscribe();
-    void msgReceived(const QByteArray &msg, const QMqttTopicName &topic);
+    void msgReceived(const QByteArray& msg, const QMqttTopicName& topic);
     void reconnect();
     void startKeepaliveTimer();
     void stopKeepaliveTimer();
-    void webswitchRequestData();
-    void webswitchParseData(int exitCode, QProcess::ExitStatus);
-    void parseMqtt(QString topic, QByteArray msg);
-    void createGet();
+    void parseMqtt(const QString& topic, const QByteArray& msg);
+    void reqWebswitchData();
+    void networkAccessManagerReplyHandler(QNetworkReply *reply);
 
 private:
-    QMqttClient mqttClient;
-    QTimer *keepaliveTimer = new QTimer;
-    QTimer *webswitchTimer = new QTimer;
-    QTimer *connectionTimer = new QTimer;
-    QTimer *receivedDataTimer = new QTimer;
-    QList<double> subValues;
-    QProcess *webswitchProcess = new QProcess;
-    SITESTATUS siteStatus = UNKNOWN;
+    QMqttClient m_mqttClient;
+    QTimer *m_keepaliveTimer = new QTimer;
+    QTimer *m_webswitchTimer = new QTimer;
+    QTimer *m_connectionTimer = new QTimer;
+    QTimer *m_receivedDataTimer = new QTimer;
+    QList<double> m_subValues;
+    SITESTATUS m_siteStatus = UNKNOWN;
     QSharedPointer<Config> config;
 
     // config cache
